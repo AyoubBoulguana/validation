6b9301f5a83d3b09deefd08a5061adeb4f3e6f73
ayoubboulguana <ayoubboulguana@gmail.com>
2025-02-11 12:02:21 +0100

    fix formValidation

--- utils/formValidation.js
+++ utils/formValidation.js
@@ -1,12 +1,8 @@
 import * as Yup from "yup";
-
-const signUpSchema = Yup.object().shape({
-  fullname: Yup.string().required("Full Name is required"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
-  password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
-    .required("Password is required"),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref("password")], "Passwords must match")
-    .required("Confirmation Password is required"),
+export const signUpSchema = Yup.object().shape({
+  fullname: Yup.String().required("You need at least 3 chars"),
+  email: Yup.String().email().required("Email is required"),
+  password: Yup.String().min(8, "Password is too short - should be 8"),
+  passwordConfirm: Yup.String().oneOf([Yup.ref("password"), null], "Passwords"),
+  Fullname: Yup.string().required("Fullname is required"),
 });
